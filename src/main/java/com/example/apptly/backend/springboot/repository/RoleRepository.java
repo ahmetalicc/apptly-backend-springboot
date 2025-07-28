c01e8714774da0f5bd1ad2997da0b54da9d2ed30
Ahmet Alıç <ahmet.alic@ogr.sakarya.edu.tr>
2025-07-28 21:48:04 +0300

    Add UserRepository, RoleRepository, and TenantRepository

---
+++ src/main/java/com/example/apptly/backend/springboot/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.apptly.backend.springboot.repository;
+
+import com.example.apptly.backend.springboot.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
