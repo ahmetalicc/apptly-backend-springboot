d3da3cec6ea07a524af09509e4032083a05d71f1
Ahmet Alıç <ahmet.alic@ogr.sakarya.edu.tr>
2025-08-12 00:18:54 +0300

    feat: Role based access added with PreAuthorize annotation on controllers. - Jwt problems are solved.

--- src/main/java/com/example/apptly/backend/springboot/controller/RoleController.java
+++ src/main/java/com/example/apptly/backend/springboot/controller/RoleController.java
@@ -8,6 +8,7 @@ import com.example.apptly.backend.springboot.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,24 +20,28 @@ public class RoleController {
 
     private final RoleService roleService;
 
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
     @PostMapping
     public ResponseEntity<ApiResponse<RoleResponse>> createRole(@RequestBody RoleRequest roleRequest){
         RoleResponse role = roleService.createRole(roleRequest);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponseUtil.success("Role has been created", role, HttpStatus.CREATED));
     }
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
     @GetMapping
     public ResponseEntity<ApiResponse<List<RoleResponse>>> getAllRoles(){
         List<RoleResponse> roles = roleService.gelAll();
         return ResponseEntity.ok(ApiResponseUtil.success("All roles fetched", roles));
     }
 
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
     @GetMapping("/byName/{name}")
     public ResponseEntity<ApiResponse<RoleResponse>> getRoleByName(@PathVariable(name = "name") String name){
         RoleResponse role = roleService.getRoleByName(name);
         return ResponseEntity.ok(ApiResponseUtil.success("Role found with name '"+ name +"'", role));
     }
 
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<String>> deleteRole(@PathVariable(name = "id") Long id){
         roleService.deleteRole(id);
